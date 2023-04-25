0f00b5044849f59134bf704f3024e21fbbd67d32
João Victor <jvfernandes.contato@gmail.com>
2023-04-25 10:18:29 -0300

    feat: implementing the function of robo print google currency conversion

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ console.log('Bem vindo ao Bot conversor');
 async function robo() {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+
+  const moedaBase = 'dolar'
+  const moedaFinal = 'real'
+
+  const URL = `https://www.google.com/search?q=${moedaBase}+para+${moedaFinal}&oq=${moedaBase}+para+${moedaFinal}&aqs=chrome..69i57j0i512l9.2465j0j7&sourceid=chrome&ie=UTF-8`
+  await page.goto(URL);
   await page.screenshot({path: 'example.png'});
 
   await browser.close();
