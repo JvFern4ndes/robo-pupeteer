f95f7aa2bfddd0bbdd3cf13f9d90451ad13bda54
João Victor <jvfernandes.contato@gmail.com>
2023-04-25 10:12:23 -0300

    feat: implementing the function of the robo to print the website

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Bem vindo ao Bot conversor')
+const puppeteer = require('puppeteer');
+
+console.log('Bem vindo ao Bot conversor');
+
+async function robo() {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await page.screenshot({path: 'example.png'});
+
+  await browser.close();
+}
+  
+robo();
